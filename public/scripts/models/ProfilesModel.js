9040f70866f42524d7bae0b058da3cc96d412c9a
Michal <michalbkl@poczta.fm>
2024-02-19 11:24:18 +0100

    store and load selected profile

--- public/scripts/models/ProfilesModel.js
+++ public/scripts/models/ProfilesModel.js
@@ -1,4 +1,5 @@
 import Observable from "../classes/Observable.js";
+import LocalStorageManager from "../utils/LocalStorageManager.js";
 import parseProfiles from "../utils/parseProfiles.js";
 
 export default class ProfilesModel extends Observable {
@@ -17,9 +18,14 @@ export default class ProfilesModel extends Observable {
 
     async loadProfiles() {
         try {
+            const stored_profile = LocalStorageManager.read("selected_profile");
             const fetched_profiles = await parseProfiles();
-
             this.profiles = fetched_profiles;
+
+            if (stored_profile) {
+                const { id } = this.profiles.find((profile) => profile.name === stored_profile.payload.name);
+                this.selectProfile(id);
+            }
         } catch (e) {
             this.profiles = [];
         }
@@ -28,10 +34,11 @@ export default class ProfilesModel extends Observable {
     }
 
     selectProfile(id) {
-        const [found_profile] = this.profiles.filter((profile) => profile.id === id);
+        const found_profile = this.profiles.find((profile) => profile.id === id);
 
         this.selected_profile = found_profile;
 
+        LocalStorageManager.write("selected_profile", this.selected_profile);
         this.onSelectedProfileUpdate(this.selected_profile);
         this.publish("selectedProfileUpdate", this.selected_profile);
     }
